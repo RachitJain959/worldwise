1e94074d873f38554cc77a041b3dcb3b145c625b
Rachit Jain <rachitjain9596@gmail.com>
2024-02-17 22:40:58 +0530

    AppNav cities countries

--- src/App.jsx
+++ src/App.jsx
@@ -17,6 +17,7 @@ function App() {
         <Route path="login" element={<Login />} />
 
         <Route path="app" element={<AppLayout />}>
+          <Route index element={<p>List of cities</p>} />
           <Route path="cities" element={<p>List of cities</p>} />
           <Route path="countries" element={<p>List of countries</p>} />
           <Route path="form" element={<p>Form</p>} />
