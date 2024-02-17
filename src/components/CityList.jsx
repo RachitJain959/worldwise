737dba3c683193cf57117c57bf161adab27e34fd
Rachit Jain <rachitjain9596@gmail.com>
2024-02-18 01:38:33 +0530

    Complete CityList Message

--- src/components/CityList.jsx
+++ src/components/CityList.jsx
@@ -1,9 +1,13 @@
 import CityItem from './CityItem';
 import styles from './CityList.module.css';
+import Message from './Message';
 import Spinner from './Spinner';
 
 function CityList({ cities, isLoading }) {
   if (isLoading) return <Spinner />;
+  if (!cities.length)
+    return <Message message={'Click on the map to add first city '} />;
+
   return (
     <div className={styles.cityList}>
       {cities.map((city) => (
