9420ae39da373d237b68ce37a8d90d5320a6c8ac
Rachit Jain <rachitjain9596@gmail.com>
2024-02-18 18:36:06 +0530

    CountryList complete-unique countries

--- src/components/CountryList.jsx
+++ src/components/CountryList.jsx
@@ -8,7 +8,13 @@ function CountryList({ cities, isLoading }) {
   if (!cities.length)
     return <Message message={'Click on the map to add first city '} />;
 
-  const countries = [];
+  const countries = cities.reduce((arr, city) => {
+    //arr-return array, city-current iterating obj. If arr of countries does not contain current
+    // city.country then return [...arr, {country, emoji}] of current city.
+    if (!arr.map((el) => el.country).includes(city.country))
+      return [...arr, { country: city.country, emoji: city.emoji }];
+    else return arr;
+  }, []);
 
   return (
     <div className={styles.countryList}>
