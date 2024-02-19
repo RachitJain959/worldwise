eed70d1bba3ff9e79f66a8f0561a4c3ffb4b2020
Rachit Jain <rachitjain9596@gmail.com>
2024-02-19 16:59:34 +0530

    Read query string

--- src/components/City.jsx
+++ src/components/City.jsx
@@ -1,4 +1,4 @@
-import { useParams } from 'react-router-dom';
+import { useParams, useSearchParams } from 'react-router-dom';
 import styles from './City.module.css';
 
 const formatDate = (date) =>
@@ -19,9 +19,19 @@ function City() {
     notes: 'My favorite city so far!',
   };
 
+  const [searchParams, setSearchParams] = useSearchParams();
+  const lat = searchParams.get('lat');
+  const lng = searchParams.get('lng');
+
   const { cityName, emoji, date, notes } = currentCity;
 
-  return <h1>City {id}</h1>;
+  return (
+    <>
+      <h1>City {id}</h1>
+      <p>lat={lat}</p>
+      <p>lng={lng}</p>
+    </>
+  );
 }
 
 export default City;
