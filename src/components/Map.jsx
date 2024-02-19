45e8cc2c4b33b55adbc2ce83fe4aab843cb45053
Rachit Jain <rachitjain9596@gmail.com>
2024-02-19 21:37:07 +0530

    useNavigate to link Map & Form

--- src/components/Map.jsx
+++ src/components/Map.jsx
@@ -1,4 +1,4 @@
-import { useSearchParams } from 'react-router-dom';
+import { useNavigate, useSearchParams } from 'react-router-dom';
 import styles from './Map.module.css';
 
 function Map() {
@@ -7,8 +7,10 @@ function Map() {
   const lat = searchParams.get('lat');
   const lng = searchParams.get('lng');
 
+  const navigate = useNavigate();
+
   return (
-    <div className={styles.mapContainer}>
+    <div className={styles.mapContainer} onClick={() => navigate('form')}>
       <h1>Map</h1>
       <p>lat={lat}</p>
       <p>lng={lng}</p>
